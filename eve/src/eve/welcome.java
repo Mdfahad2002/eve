c305de3c6f27eb46cc7d9e31d482b73ca2065ba1
Rayeez <mdrayeez94@gmail.com>
2022-03-08 18:12:18 +0530

    my first commit

---
+++ eve/src/eve/welcome.java
@@ -0,0 +1,14 @@
+package eve;
+
+public class welcome {
+
+	public welcome() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
